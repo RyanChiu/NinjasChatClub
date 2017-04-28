8270e3f1a18487b1e63437f801d3626724c58787
ninja <ninja@ninjaschatclub.mia.fsx.com>
2017-04-27 22:17:20 -0400

    show links encrypted.

--- app/vendors/extrakits.inc.php
+++ app/vendors/extrakits.inc.php
@@ -19,12 +19,18 @@
 	 * critical time to change biweek into 1-15 or 16-end of a month
 	 */
 	define("INTOHALFMONTHLYBIWEEKDAY", '2016-12-01');
+	
+	/*
+	 * key string that will be used for encrypt and decrypt
+	 */
+	define("LINKGENKEY", "ohmyOHMY890$%^");
+
 	/*
 	 * routines area
 	 */
 	//date_default_timezone_set("Asia/Manila");
 	date_default_timezone_set("EST5EDT");
-
+	
 	/*
 	 * functions area
 	 */
@@ -403,5 +409,60 @@
 			$periods += array($biweek => "[2W]$biweek");
 		}
 		return $periods;
+	}
+	
+	/**
+	 * encrypt, decrypt
+	 */
+	function encrypt($data, $key)
+	{
+		$key    =    md5($key);
+		$x        =    0;
+		$len    =    strlen($data);
+		$l        =    strlen($key);
+		for ($i = 0; $i < $len; $i++)
+		{
+			if ($x == $l)
+			{
+				$x = 0;
+			}
+			$char .= $key{$x};
+			$x++;
+		}
+		for ($i = 0; $i < $len; $i++)
+		{
+			$str .= chr(ord($data{$i}) + (ord($char{$i})) % 256);
+		}
+		return base64_encode($str);
+	}
+	
+	function decrypt($data, $key)
+	{
+		$key = md5($key);
+		$x = 0;
+		$data = base64_decode($data);
+		$len = strlen($data);
+		$l = strlen($key);
+		for ($i = 0; $i < $len; $i++)
+		{
+			if ($x == $l)
+			{
+				$x = 0;
+			}
+			$char .= substr($key, $x, 1);
+			$x++;
+		}
+		for ($i = 0; $i < $len; $i++)
+		{
+			if (ord(substr($data, $i, 1)) < ord(substr($char, $i, 1)))
+			{
+				$str .= chr((ord(substr($data, $i, 1)) + 256) - ord(substr($char, $i, 1)));
+			}
+			else
+			{
+				$str .= chr(ord(substr($data, $i, 1)) - ord(substr($char, $i, 1)));
+			}
+		}
+		return $str;
 	}
 ?>
