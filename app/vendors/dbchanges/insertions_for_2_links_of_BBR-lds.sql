ccd974afae8551afbeec12760553b36ec0092701
ray <ray@debian-devz.debian-devz>
2017-05-17 02:07:47 +0800

    db changes for the last commit.

---
+++ app/vendors/dbchanges/insertions_for_2_links_of_BBR-lds.sql
@@ -0,0 +1,7 @@
+INSERT INTO `types` (`id`, `siteid`, `typename`, `typealias`, `url`, `status`) VALUES (NULL, '11', ' L5_$1.95_Trial', ' L5_$1.95_Trial', 'http://cdspot.circularplug.com/tools/landers/st/007eih/index.php?ofid=44&pgm=18&a_aid=SC123B&a_bid=af5d4b8a&x_agent=__CAM__&chan=__CAM__', '1');
+
+INSERT INTO `types` (`id`, `siteid`, `typename`, `typealias`, `url`, `status`) VALUES (NULL, '11', ' L6_Free_Trial', ' L6_Free_Trial', 'http://cdspot.circularplug.com/tools/landers/st/007eih/index.php?ofid=44&a_aid=SC123B&a_bid=2f60fc2d
+&x_agent=__CAM__&chan=__CAM__', '1');
+
+INSERT INTO `fees` (`id`, `typeid`, `price`, `earning`, `start`, `end`) VALUES (NULL, '38', '15.00', '23.00', '0000-00-00 00:00:00', '3999-01-01 00:00:00');
+INSERT INTO `fees` (`id`, `typeid`, `price`, `earning`, `start`, `end`) VALUES (NULL, '39', '15.00', '23.00', '0000-00-00 00:00:00', '3999-01-01 00:00:00');
